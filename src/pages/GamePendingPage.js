8adf1d360605d5cc73925afac4d862fef9146aec
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-15 17:10:07 -0500

    adding funds to fundGame

--- src/pages/GamePendingPage.js
+++ src/pages/GamePendingPage.js
@@ -70,7 +70,7 @@ const GamePendingPage = () => {
         }
 
         try {
-            const entryFee = Web3.utils.toWei('.0001', 'ether');
+            const entryFee = Web3.utils.toWei('.001', 'ether');
             console.log("entryFee", entryFee)
 
             const nonce = await Web3.eth.getTransactionCount(walletAddress);
