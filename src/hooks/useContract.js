d91a18d86c3f2c0b577ff7bbc756068c7c18f99a
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-19 02:34:59 -0500

    update useContract

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,39 +1,26 @@
-import { useEffect, useState } from 'react';
-import Web3 from 'web3';
+import { useState, useEffect } from 'react';
+const { ethers } = require('ethers');
 
-const useContract = (contractAddress, abi) => {
-    console.log('contractAddress in useContract Hook:', contractAddress);
-  const [contractInstance, setContractInstance] = useState(null);
-  const [events, setEvents] = useState([]);
+const useContract = (abi, contractAddress) => {
+    const [contractInstance, setContractInstance] = useState(null);
 
-  useEffect(() => {
-    if (contractAddress && abi) {
-        console.log('Initializing contract instance...')
-      // Initialize contract instance
-      const web3 = new Web3(window.ethereum); // Assuming you're using MetaMask
-      const instance = new web3.eth.Contract(abi, contractAddress);
-      setContractInstance(instance);
-
-      console.log('Subscribing to events...');
-
-      // Subscribe to events
-      const eventListener = instance.events.allEvents((error, event) => {
-        if (error) {
-          console.error('Error fetching events:', error);
-          return;
+    useEffect(() => {
+        if (!abi || !contractAddress) {
+            console.error('ABI or contract address is missing.');
+            return;
         }
-        console.log('Received event:', event);
-        setEvents((prevEvents) => [...prevEvents, event]);
-      });
 
-      // Cleanup function
-      return () => {
-        eventListener.unsubscribe();        
-      };
-    }
-  }, [contractAddress, abi]);
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, abi, signer);
+            setContractInstance(contract);
+        } catch (error) {
+            console.error('Error initializing contract instance:', error);
+        }
+    }, [abi, contractAddress]);
 
-  return { contractInstance, events };
-}
+    return contractInstance;
+};
 
 export default useContract;
