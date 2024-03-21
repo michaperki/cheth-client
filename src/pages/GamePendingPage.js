950d95f81f3c95cfb5a0b8d541528df41e4eb3e9
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-20 23:38:03 -0500

    tnkr

--- src/pages/GamePendingPage.js
+++ src/pages/GamePendingPage.js
@@ -168,7 +168,7 @@ const GamePendingPage = () => {
             <div className="max-w-md w-full p-8 bg-white rounded shadow-lg">
                 <h1 className="text-3xl font-semibold mb-4">Game Pending</h1>
                 {loading && <p>Loading...</p>}
-                {gameInfo && (gameInfo.state === 2 || gameInfo.state === 3) && (
+                {gameInfo && (parseInt(gameInfo.state) === 2 || parseInt(gameInfo.state) === 3) && (
                     <div>
                         <p>Game is ready. Contract address: {gameInfo.contract_address}</p>
                         <div>
