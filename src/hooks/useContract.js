46a21df74427afb8b59c5d721a08b3e88f1d3bdf
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-27 02:36:45 -0500

    roll back

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,61 +1,26 @@
 import { useState, useEffect } from 'react';
-import Web3 from 'web3';
-import Chess from '../abis/Chess.json';
-import { useSDK } from "@metamask/sdk-react";
+const { ethers } = require('ethers');
 
-const useContract = (gameId, walletAddress) => {
-    const { sdk, provider } = useSDK();
+const useContract = (abi, contractAddress) => {
     const [contractInstance, setContractInstance] = useState(null);
-    const [contractAddress, setContractAddress] = useState(null);
-    const [contractBalance, setContractBalance] = useState(0);
-
-    const web3 = new Web3(provider);
 
     useEffect(() => {
-        if (provider && contractAddress) {
-            const contract = new web3.eth.Contract(Chess.abi, contractAddress);
+        if (!abi || !contractAddress) {
+            console.error('ABI or contract address is missing.');
+            return;
+        }
+
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, abi, signer);
             setContractInstance(contract);
-        }
-    }, [provider, contractAddress]);
-
-    const joinGame = async () => {
-        try {
-            if (!connected) {
-                await sdk.requestPermissions({ eth_accounts: {} });
-            }
-            if (!contractInstance) {
-                throw new Error('Contract instance not available');
-            }
-
-            const entryFeeInWei = await contractInstance.methods.getEntryFee().call();
-            console.log('Entry fee in wei:', entryFeeInWei);
-            console.log('contract methods:', contractInstance.methods);
-            const tx = await contractInstance.methods.joinGame().send({
-                from: walletAddress,
-                value: entryFeeInWei,
-                gas: 3000000
-            });
-            // setGameInfo(prev => ({ ...prev, transactionHash: tx.transactionHash }));
         } catch (error) {
-            console.error('Error:', error);
+            console.error('Error initializing contract instance:', error);
         }
-    }
+    }, [abi, contractAddress]);
 
-    const cancelGame = async () => {
-        try {
-            await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/cancelGame`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ gameId })
-            });
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
-
-    return { contractInstance, contractAddress, contractBalance, joinGame, cancelGame };
-}
+    return contractInstance;
+};
 
 export default useContract;
