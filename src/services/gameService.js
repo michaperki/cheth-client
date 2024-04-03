be1494de21802c6d930e53226e6375d0f97d6808
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-04-02 23:28:03 -0500

    clean up

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,31 +1,3 @@
-// // Used to fetch user data from the server
-
-// const getUser = async (walletAddress) => {
-//     try {
-//       const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/user/getUser`, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ walletAddress })
-//       });
-  
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch data');
-//       }
-  
-//       const data = await response.json();
-//       console.log('User data:', data);
-//       return data;
-//     } catch (error) {
-//       console.error('Error fetching user data:', error);
-//       throw error;
-//     }
-//   };
-  
-//   export default getUser;
-  
-
 const getGameCount = async () => {
     try {
         const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/getGameCount`);
