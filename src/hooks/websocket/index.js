23b937f838d62a152da7e2a77a229e9486b7f63b
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-04-09 21:32:36 -0500

    tnkr

--- src/hooks/websocket/index.js
+++ src/hooks/websocket/index.js
@@ -1,4 +1,4 @@
-export { default as useGameWebsocket } from './useGameWebsocket';
+export { default as useGameWebsocket } from './UseGameWebsocket';
 export { default as useGamePendingWebsocket } from './useGamePendingWebsocket';
 export { default as useDashboardWebsocket } from './useDashboardWebsocket';
 export { default as useWebSocket } from './useWebsocket';
