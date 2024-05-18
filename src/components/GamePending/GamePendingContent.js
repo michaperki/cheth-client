db0ca97b65229c0e608c30a6c3ee83e2fd8d7da9
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-05-18 11:17:33 -0500

    style game pending

--- src/components/GamePending/GamePendingContent.js
+++ src/components/GamePending/GamePendingContent.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Button, Typography, Box } from '@mui/material';
-import { NumberDisplay, MatchUpPodium } from '../game';
-import Web3 from 'web3';
-import './GamePendingContent.css'; // Import your CSS file
+import React from "react";
+import { Button, Typography, Box } from "@mui/material";
+import { NumberDisplay, MatchUpPodium } from "../game";
+import Web3 from "web3";
+import "./GamePendingContent.css"; // Import your CSS file
 
 const GamePendingContent = ({
   gameInfo,
@@ -12,40 +12,56 @@ const GamePendingContent = ({
   ethToUsdRate,
   hasPlayerJoined,
   joinGame,
-  cancelGame
+  cancelGame,
 }) => {
-    return (
-        <Box className="game-pending-content">
-            <Typography variant="h3" className="game-pending-title">Game Pending</Typography>
-            <Typography className="game-id">Game ID: {gameInfo.game_id}</Typography>
+  return (
+    <Box className="game-pending-content">
+      <Typography variant="h3" className="game-pending-title">
+        Game Pending
+      </Typography>
+      <Typography className="game-id">Game ID: {gameInfo.game_id}</Typography>
 
-
-            {player_one && player_two && gameInfo.time_control && (
-                <Box sx={{ marginBottom: 3 }}>
-                    <MatchUpPodium 
-                        playerOne={player_one} 
-                        playerTwo={player_two} 
-                        gameInfo={gameInfo}
-                        timeControl={gameInfo.time_control} 
-                    />
-                </Box>
-            )}
-            
-            <NumberDisplay amount={Web3.utils.fromWei(contractBalance, 'ether') * ethToUsdRate} />
-
-            {hasPlayerJoined ? (
-                <Typography className="waiting-message">Waiting for opponent to join</Typography>
-            ) : (
-                <Button onClick={joinGame} variant="contained" color="primary" className="game-pending-button">
-                    Join Game
-                </Button>
-            )}
-
-            <Button onClick={cancelGame} variant="contained" color="error" className="game-pending-button">
-                Cancel Game
-            </Button>
+      {player_one && player_two && gameInfo.time_control && (
+        <Box sx={{ marginBottom: 3 }}>
+          <MatchUpPodium
+            playerOne={player_one}
+            playerTwo={player_two}
+            gameInfo={gameInfo}
+            timeControl={gameInfo.time_control}
+          />
         </Box>
-    );
+      )}
+
+      <NumberDisplay
+        amount={Web3.utils.fromWei(contractBalance, "ether") * ethToUsdRate}
+      />
+
+      {hasPlayerJoined ? (
+        <Typography className="waiting-message">
+          Waiting for opponent to join
+        </Typography>
+      ) : (
+        <Box className="button-container">
+          <Button
+            onClick={joinGame}
+            variant="contained"
+            color="primary"
+            className="game-pending-button"
+          >
+            Join Game
+          </Button>
+          <Button
+            onClick={cancelGame}
+            variant="contained"
+            color="error"
+            className="game-pending-button"
+          >
+            Cancel Game
+          </Button>
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default GamePendingContent;
