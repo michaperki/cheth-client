88e68a720052dee99b6afdb8c50726d931236f16
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-05-18 19:52:28 -0500

    report issue route

--- src/hooks/useGameActions.js
+++ src/hooks/useGameActions.js
@@ -1,136 +1,164 @@
 const useGameActions = (gameId, userInfo, handleFetchGameInfo, gameUrl) => {
-    const handleJoinGame = () => {
-        // semd them to the lichess url
-        window
-            .open(`https://lichess.org/${gameUrl}`, '_blank')
-            .focus();
+  const handleJoinGame = () => {
+    // semd them to the lichess url
+    window.open(`https://lichess.org/${gameUrl}`, "_blank").focus();
+  };
+
+  const handleReportGameOver = async () => {
+    // Logic to report game over
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/game/reportGameOver`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ gameId }),
+        },
+      );
+      if (response.ok) {
+        handleFetchGameInfo();
+      } else {
+        throw new Error("Failed to report game over");
+      }
+    } catch (error) {
+      console.error("Error reporting game over:", error);
     }
+  };
 
-    const handleReportGameOver = async () => {
-        // Logic to report game over
-        try {
-            const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/reportGameOver`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ gameId })
-            });
-            if (response.ok) {
-                handleFetchGameInfo();
-            } else {
-                throw new Error('Failed to report game over');
-            }
-        } catch (error) {
-            console.error('Error reporting game over:', error);
-        }
-    };
-
-    const handleReportIssue = () => {
-        // Logic to report an issue
-        alert('Issue reported!');
-    };
-
-    const handleRematch = () => {
-        console.log('Rematching...');
-        console.log('gameId', gameId);
-        console.log('userId', userInfo.user_id);
-        try {
-            const response = fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/requestRematch`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ userId: userInfo.user_id, gameId })
-            });
-
-            if (!response.ok) {
-                throw new Error('Failed to rematch');
-            }
-
-            console.log('Rematch successful!');
-        }
-        catch (error) {
-            console.error('Error rematching:', error);
-        }
+  const handleReportIssue = async () => {
+    // Logic to report an issue and lock the game
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/game/reportIssue`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ gameId: gameId }),
+        },
+      );
+      if (response.ok) {
+        alert("Issue reported and game locked!");
+      } else {
+        throw new Error("Failed to report issue");
+      }
+    } catch (error) {
+      console.error("Error reporting issue:", error);
+      alert("Failed to report issue");
     }
+  };
 
-    const handleAcceptRematch = () => {
-        console.log('Accepting rematch...');
+  const handleRematch = () => {
+    console.log("Rematching...");
+    console.log("gameId", gameId);
+    console.log("userId", userInfo.user_id);
+    try {
+      const response = fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/game/requestRematch`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId: userInfo.user_id, gameId }),
+        },
+      );
 
-        try {
-            const response = fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/acceptRematch`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ userId: userInfo.user_id, gameId })
-            });
+      if (!response.ok) {
+        throw new Error("Failed to rematch");
+      }
 
-            if (!response.ok) {
-                throw new Error('Failed to accept rematch');
-            }
-
-            console.log('Rematch accepted!');
-        }
-        catch (error) {
-            console.error('Error accepting rematch:', error);
-        }
+      console.log("Rematch successful!");
+    } catch (error) {
+      console.error("Error rematching:", error);
     }
+  };
 
-    const handleDeclineRematch = () => {
-        console.log('Declining rematch...');
-        try {
-            const response = fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/declineRematch`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ userId: userInfo.user_id, gameId })
-            });
+  const handleAcceptRematch = () => {
+    console.log("Accepting rematch...");
 
-            if (!response.ok) {
-                throw new Error('Failed to decline rematch');
-            }
+    try {
+      const response = fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/game/acceptRematch`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId: userInfo.user_id, gameId }),
+        },
+      );
 
-            console.log('Rematch declined!');
-        }
-        catch (error) {
-            console.error('Error declining rematch:', error);
-        }
+      if (!response.ok) {
+        throw new Error("Failed to accept rematch");
+      }
+
+      console.log("Rematch accepted!");
+    } catch (error) {
+      console.error("Error accepting rematch:", error);
     }
+  };
 
-    const handleCancelRematch = () => {
-        console.log('Canceling rematch...');
-        try {
-            const response = fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/game/cancelRematch`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ userId: userInfo.user_id, gameId })
-            });
+  const handleDeclineRematch = () => {
+    console.log("Declining rematch...");
+    try {
+      const response = fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/game/declineRematch`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId: userInfo.user_id, gameId }),
+        },
+      );
 
-            if (!response.ok) {
-                throw new Error('Failed to cancel rematch');
-            }
+      if (!response.ok) {
+        throw new Error("Failed to decline rematch");
+      }
 
-            console.log('Rematch canceled!');
-        }
-        catch (error) {
-            console.error('Error canceling rematch:', error);
-        }
+      console.log("Rematch declined!");
+    } catch (error) {
+      console.error("Error declining rematch:", error);
     }
+  };
 
-    return {
-        handleJoinGame,
-        handleReportGameOver,
-        handleReportIssue,
-        handleRematch,
-        handleAcceptRematch,
-        handleDeclineRematch,
-        handleCancelRematch
-    };
+  const handleCancelRematch = () => {
+    console.log("Canceling rematch...");
+    try {
+      const response = fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/game/cancelRematch`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId: userInfo.user_id, gameId }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to cancel rematch");
+      }
+
+      console.log("Rematch canceled!");
+    } catch (error) {
+      console.error("Error canceling rematch:", error);
+    }
+  };
+
+  return {
+    handleJoinGame,
+    handleReportGameOver,
+    handleReportIssue,
+    handleRematch,
+    handleAcceptRematch,
+    handleDeclineRematch,
+    handleCancelRematch,
+  };
 };
 
 export default useGameActions;
