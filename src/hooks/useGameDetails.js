310973dc07bbe4e01fd49875ff02c11bf5513fbf
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-08-28 16:02:54 -0500

    update useGameDetails hook to handle game state 5

--- src/hooks/useGameDetails.js
+++ src/hooks/useGameDetails.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 const useGameDetails = (gameId, userInfo) => {
     const [gameInfo, setGameInfo] = useState(null);
@@ -17,7 +17,6 @@ const useGameDetails = (gameId, userInfo) => {
             }
             const data = await response.json();
             setGameInfo(data);
-            // Additional logic to set game state can be added here
 
             if (data.player1_id) {
                 handleFetchPlayerInfo(data.player1_id, setPlayerOne);
@@ -25,6 +24,13 @@ const useGameDetails = (gameId, userInfo) => {
 
             if (data.player2_id) {
                 handleFetchPlayerInfo(data.player2_id, setPlayerTwo);
+            }
+
+            // Check if the game is over
+            if (data.state === "5") {
+                setGameOver(true);
+                setWinner(data.winner ? await getWinnerUsername(data.winner) : 'Draw');
+                setWinnerPaid(true);
             }
 
         } catch (error) {
@@ -51,11 +57,38 @@ const useGameDetails = (gameId, userInfo) => {
         }
     }, []);
 
+    const getWinnerUsername = async (winnerId) => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/user/${winnerId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ userId: winnerId })
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch winner information');
+            }
+            const data = await response.json();
+            return data.username;
+        } catch (error) {
+            console.error('Error fetching winner information:', error);
+            return 'Unknown';
+        }
+    };
+
     const handleCloseSnackbar = () => {
         setSnackbarInfo({ ...snackbarInfo, open: false });
     };
 
-    // Further functionalities like handling websocket messages can be added here
+    useEffect(() => {
+        handleFetchGameInfo();
+        // Set up an interval to fetch game info every 10 seconds
+        const intervalId = setInterval(handleFetchGameInfo, 10000);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(intervalId);
+    }, [handleFetchGameInfo]);
 
     return {
         gameInfo,
