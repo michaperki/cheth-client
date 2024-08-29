583aee74eef3c14d14142e10f703eabe43c0cdc1
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-08-29 12:28:28 -0500

    reformatting

--- src/hooks/websocket/useDashboardWebsocket.js
+++ src/hooks/websocket/useDashboardWebsocket.js
@@ -1,8 +1,8 @@
-import { useRef, useState, useEffect } from 'react';
-import useWebSocket from './useWebsocket';
-import { useNavigate } from 'react-router-dom';
-import Web3 from 'web3';
-import { toast } from 'react-toastify';
+import { useRef, useState, useEffect } from "react";
+import useWebSocket from "./useWebsocket";
+import { useNavigate } from "react-router-dom";
+import Web3 from "web3";
+import { toast } from "react-toastify";
 
 const useDashboardWebsocket = ({ ethToUsdRate, userInfo }) => {
   const [searchingForOpponent, setSearchingForOpponent] = useState(false);
@@ -11,31 +11,35 @@ const useDashboardWebsocket = ({ ethToUsdRate, userInfo }) => {
   const timeoutIdRef = useRef(null);
 
   const handleDashboardPageWebSocketMessage = (message) => {
-    console.log('Received message in DashboardPage:', message);
+    console.log("Received message in DashboardPage:", message);
     const messageData = JSON.parse(message);
-    console.log('messageData', messageData);
+    console.log("messageData", messageData);
 
     if (messageData.type === "START_GAME") {
-      console.log('Game started:', messageData);
+      console.log("Game started:", messageData);
       setOpponentFound(true);
       clearSearchTimeout();
     }
 
     if (messageData.type === "CONTRACT_READY") {
-      console.log('Game contract ready:', messageData);
+      console.log("Game contract ready:", messageData);
       clearSearchTimeout();
       setSearchingForOpponent(false);
       navigate(`/game-pending/${messageData.gameId}`);
     }
 
     if (messageData.type === "FUNDS_TRANSFERRED") {
-      const transferredInEth = Web3.utils.fromWei(messageData.amount, 'ether');
+      const transferredInEth = Web3.utils.fromWei(messageData.amount, "ether");
       const transferredInUsd = (transferredInEth * ethToUsdRate).toFixed(2);
       toast.success(`You received $${transferredInUsd}.`);
     }
   };
 
-  const { socket, onlineUsersCount } = useWebSocket(handleDashboardPageWebSocketMessage, userInfo?.user_id, ['ONLINE_USERS_COUNT']);
+  const { socket, onlineUsersCount } = useWebSocket(
+    handleDashboardPageWebSocketMessage,
+    userInfo?.user_id,
+    ["ONLINE_USERS_COUNT"],
+  );
 
   const clearSearchTimeout = () => {
     if (timeoutIdRef.current) {
@@ -46,16 +50,21 @@ const useDashboardWebsocket = ({ ethToUsdRate, userInfo }) => {
 
   useEffect(() => {
     if (searchingForOpponent && socket) {
-      console.log('Setting timeout for search timeout');
+      console.log("Setting timeout for search timeout");
       timeoutIdRef.current = setTimeout(() => {
-        console.log('Search timed out');
-        toast.error('Search timed out.');
+        console.log("Search timed out");
+        toast.error("Search timed out.");
         setSearchingForOpponent(false);
         if (socket.readyState === WebSocket.OPEN) {
-          console.log('Sending CANCEL_SEARCH due to timeout');
-          socket.send(JSON.stringify({ type: 'CANCEL_SEARCH', userId: userInfo?.user_id }));
+          console.log("Sending CANCEL_SEARCH due to timeout");
+          socket.send(
+            JSON.stringify({
+              type: "CANCEL_SEARCH",
+              userId: userInfo?.user_id,
+            }),
+          );
         } else {
-          console.log('Socket is not open. Cannot send CANCEL_SEARCH');
+          console.log("Socket is not open. Cannot send CANCEL_SEARCH");
         }
       }, 30000); // 30 seconds
     }
@@ -64,14 +73,16 @@ const useDashboardWebsocket = ({ ethToUsdRate, userInfo }) => {
   }, [searchingForOpponent, socket, userInfo]);
 
   const cancelSearch = () => {
-    console.log('Cancel search initiated');
+    console.log("Cancel search initiated");
     clearSearchTimeout();
 
     if (socket && socket.readyState === WebSocket.OPEN) {
-      console.log('Sending CANCEL_SEARCH message');
-      socket.send(JSON.stringify({ type: 'CANCEL_SEARCH', userId: userInfo?.user_id }));
+      console.log("Sending CANCEL_SEARCH message");
+      socket.send(
+        JSON.stringify({ type: "CANCEL_SEARCH", userId: userInfo?.user_id }),
+      );
     } else {
-      console.log('Socket is not open or undefined. Cannot send CANCEL_SEARCH');
+      console.log("Socket is not open or undefined. Cannot send CANCEL_SEARCH");
     }
 
     setSearchingForOpponent(false);
@@ -82,7 +93,7 @@ const useDashboardWebsocket = ({ ethToUsdRate, userInfo }) => {
     searchingForOpponent,
     opponentFound,
     setSearchingForOpponent,
-    cancelSearch
+    cancelSearch,
   };
 };
 
