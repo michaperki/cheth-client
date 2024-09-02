6dcd6e5ef2088fed217f10e15a3859c70ea02936
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-02 13:04:24 -0500

    add slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
+// src/store/index.js
+
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import gameReducer from './slices/gameSlice';
+import authReducer from './slices/authSlice';
+import ethereumPriceReducer from './slices/ethereumPriceSlice';
+import gameSettingsReducer from './slices/gameSettingsSlice';
+import onlineUsersReducer from './slices/onlineUsersSlice';
+import gameStatsReducer from './slices/gameStatsSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     game: gameReducer,
+    auth: authReducer,
+    ethereumPrice: ethereumPriceReducer,
+    gameSettings: gameSettingsReducer,
+    onlineUsers: onlineUsersReducer,
+    gameStats: gameStatsReducer,
   },
 });
