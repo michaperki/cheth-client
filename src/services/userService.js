3fca2243f6a536e128c172584ccfd4aa59225437
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-05 10:45:01 -0500

    oh jeez rollback!

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,27 @@
+// Used to fetch user data from the server
+
 const getUser = async (walletAddress) => {
-  try {
-    const cachedUser = localStorage.getItem(`userInfo_${walletAddress}`);
-    if (cachedUser) {
-      const { data, timestamp } = JSON.parse(cachedUser);
-      if (Date.now() - timestamp < 30 * 60 * 1000) { // 30 minutes
-        console.log('User data from cache:', data);
-        return data;
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/user/getUser`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ walletAddress })
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
       }
+  
+      const data = await response.json();
+      console.log('User data:', data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      throw error;
     }
-
-    const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/user/getUser`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ walletAddress })
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const data = await response.json();
-    console.log('User data:', data);
-    localStorage.setItem(`userInfo_${walletAddress}`, JSON.stringify({ data, timestamp: Date.now() }));
-    return data;
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
+  };
+  
+  export default getUser;
+  
