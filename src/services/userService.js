dd4d635a9a0db2e02c0ab5a78816c59db66ce4c1
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-05 11:09:16 -0500

    user service simple cache

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,27 +1,37 @@
-// Used to fetch user data from the server
+let userCache = {};
 
 const getUser = async (walletAddress) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/user/getUser`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ walletAddress })
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-  
-      const data = await response.json();
-      console.log('User data:', data);
-      return data;
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-      throw error;
+  try {
+    // Check if we have cached data and it's less than 5 minutes old
+    if (userCache[walletAddress] && (Date.now() - userCache[walletAddress].timestamp < 5 * 60 * 1000)) {
+      console.log('User data from cache:', userCache[walletAddress].data);
+      return userCache[walletAddress].data;
     }
-  };
-  
-  export default getUser;
-  
+
+    const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/user/getUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ walletAddress })
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    console.log('User data:', data);
+    
+    // Cache the data
+    userCache[walletAddress] = {
+      data: data,
+      timestamp: Date.now()
+    };
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+};
