995962f07c0689d4a6cb575f359f0bae21fda343
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-05 10:45:57 -0500

    more conservative tinker to ethPriceContext

--- src/contexts/EthereumPriceContext.js
+++ src/contexts/EthereumPriceContext.js
@@ -1,29 +1,33 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
-//
-// Create a context
+import React, { createContext, useContext, useEffect, useState, useCallback } from 'react';
+
 const EthereumPriceContext = createContext();
 
-// Create a provider component
 export const EthereumPriceProvider = ({ children }) => {
   const [ethToUsdRate, setEthToUsdRate] = useState(0);
 
-  useEffect(() => {
-    const fetchEthToUsdRate = async () => {
-      try {
-        const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/crypto/ethToUsd`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch ETH to USD conversion rate');
-        }
-        const data = await response.json();
-        console.log('ETH to USD conversion rate:', data);
-        setEthToUsdRate(data);
-      } catch (error) {
-        console.error('Error fetching ETH to USD conversion rate:', error);
+  const fetchEthToUsdRate = useCallback(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/crypto/ethToUsd`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch ETH to USD conversion rate');
       }
-    };
+      const data = await response.json();
+      setEthToUsdRate(data);
+      console.log('ETH to USD conversion rate:', data);
+    } catch (error) {
+      console.error('Error fetching ETH to USD conversion rate:', error);
+    }
+  }, []);
 
+  useEffect(() => {
     fetchEthToUsdRate();
-  }, []); // Fetch the rate only once when the component mounts
+    
+    const intervalId = setInterval(() => {
+      fetchEthToUsdRate();
+    }, 5 * 60 * 1000); // Fetch every 5 minutes
+
+    return () => clearInterval(intervalId);
+  }, [fetchEthToUsdRate]);
 
   return (
     <EthereumPriceContext.Provider value={ethToUsdRate}>
@@ -32,7 +36,6 @@ export const EthereumPriceProvider = ({ children }) => {
   );
 };
 
-// Custom hook to consume the Ethereum price context
 export const useEthereumPrice = () => {
   return useContext(EthereumPriceContext);
 };
