a95a316476d5d2f2346433bfb1b8fa32bc8fd3f3
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-05 14:51:04 -0500

    update import syntax for app.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from 'store';
 import { MetaMaskProvider } from "@metamask/sdk-react";
-import App from './App';
+import App from 'App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
